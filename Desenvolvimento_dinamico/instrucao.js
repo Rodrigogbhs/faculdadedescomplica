0b29aa62f7781c6931f6c243a729b5766c92a1dc
Rodrigo Guimarães <rodrigogbhs@hotmail.com>
2024-09-01 18:53:43 -0300

    add projetosFaculdade

---
+++ Desenvolvimento_dinamico/instrucao.js
@@ -0,0 +1,17 @@
+var país = Brasil;
+
+if(país != "Brasil"){
+    console.log("voce é estrangeiro");
+}else{
+    console.log("voce é brasieiro");
+}
+
+var idade = 22;
+
+if(idade < 16){
+    console.log("voce nao vota");
+}else if (idade <18 ou 65>){
+    console.log("voto opcional");
+}else (idade >=18){
+    console.log("voto obrigatorio");
+}
